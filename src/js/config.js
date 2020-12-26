bc72c44c89085b002e6fe76af9d1413810e34beb
krisjanis <krisjanis@emitapps.com>
2020-12-26 19:09:32 +0200

    config

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-// export const API_URL =
-//   "https://cors-anywhere.herokuapp.com/https://www.delfi.lv/misc/task_2020/";
-export const API_URL = "./data.json";
+export const API_URL =
+  "https://cors-anywhere.herokuapp.com/https://www.delfi.lv/misc/task_2020/";
+// export const API_URL = "./data.json";
 export const CHANNELS = { 0: "Visi", 1: "Delfi", 7: "Delfi Pluss", 40: "MVP" };
